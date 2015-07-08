3a48e639877f895d1d7fddd503e0e10d95d9f93c
wangbintao1992 <541437863@qq.com>
2015-07-08 17:57:42 +0800

    test

--- src/main/java/com/cube/controller/CubeController.java
+++ src/main/java/com/cube/controller/CubeController.java
@@ -1,13 +1,15 @@
 package com.cube.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class CubeController {
-	
 	@RequestMapping("test")
-	public void test(){
-		System.out.println("success");
+	public void test(HttpServletRequest request){
+		String[] str = request.getParameterValues("t");
+		String s = request.getParameter("t");
 	}
 }
